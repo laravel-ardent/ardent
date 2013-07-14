2976de519d96147faf9b79cab3375379b5bb37df
Igor Santos <igorsantos07@gmail.com>
2013-07-14 18:35:23 -0300

    Implementing Ardent:: to have less relational clutter in models

--- src/LaravelBook/Ardent/Ardent.php
+++ src/LaravelBook/Ardent/Ardent.php
@@ -131,6 +131,67 @@ abstract class Ardent extends Model {
     protected static $validationFactory;
 
     /**
+     * Can be used to ease declaration of relationships in Ardent models.
+     * Follows closely the behavior of the relation methods used by Eloquent, but packing them into an indexed array
+     * with relation constants make the code less cluttered.
+     *
+     * It should be declared with camel-cased keys as the relation name, and value being a mixed array with the
+     * relation constant being the first (0) value, the second (1) being the classname and the next ones (optionals)
+     * having named keys indicating the other arguments of the original methods: 'foreignKey' (belongsTo, hasOne,
+     * belongsToMany and hasMany); 'table' and 'otherKey' (belongsToMany only); 'name', 'type' and 'id' (specific for
+     * morphTo, morphOne and morphMany).
+     * Exceptionally, the relation type MORPH_TO does not include a classname, following the method declaration of
+     * {@link \Illuminate\Database\Eloquent\Model::morphTo}.
+     *
+     * Example:
+     * <code>
+     * class Order extends Ardent {
+     *     protected static $relations = array(
+     *         'items'    => array(self::HAS_MANY, 'Item'),
+     *         'owner'    => array(self::HAS_ONE, 'User', 'foreignKey' => 'user_id'),
+     *         'pictures' => array(self::MORPH_MANY, 'Picture', 'name' => 'imageable')
+     *     );
+     * }
+     * </code>
+     *
+     * @see \Illuminate\Database\Eloquent\Model::hasOne
+     * @see \Illuminate\Database\Eloquent\Model::hasMany
+     * @see \Illuminate\Database\Eloquent\Model::belongsTo
+     * @see \Illuminate\Database\Eloquent\Model::belongsToMany
+     * @see \Illuminate\Database\Eloquent\Model::morphTo
+     * @see \Illuminate\Database\Eloquent\Model::morphOne
+     * @see \Illuminate\Database\Eloquent\Model::morphMany
+     *
+     * @var array
+     */
+    protected static $relationsData = array();
+
+    const HAS_ONE = 'hasOne';
+
+    const HAS_MANY = 'hasMany';
+
+    const BELONGS_TO = 'belongsTo';
+
+    const BELONGS_TO_MANY = 'belongsToMany';
+
+    const MORPH_TO = 'morphTo';
+
+    const MORPH_ONE = 'morphOne';
+
+    const MORPH_MANY = 'morphMany';
+
+    /**
+     * Array of relations used to verify arguments used in the {@link $relationsData}
+     * 
+     * @var array
+     */
+    protected static $relationTypes = array(
+        self::HAS_ONE, self::HAS_MANY,
+        self::BELONGS_TO, self::BELONGS_TO_MANY,
+        self::MORPH_TO, self::MORPH_ONE, self::MORPH_MANY
+    );
+
+    /**
      * Create a new Ardent model instance.
      *
      * @param array $attributes
@@ -140,6 +201,115 @@ abstract class Ardent extends Model {
 
         parent::__construct($attributes);
         $this->validationErrors = new MessageBag;
+    }
+
+    /**
+     * Looks for the relation in the {@link $relationsData} array and does the correct magic as Eloquent would require
+     * inside relation methods. For more information, read the documentation of the mentioned property.
+     *
+     * @param $relationName the relation key, camel-case version
+     * @return \Illuminate\Database\Eloquent\Relations\Relation
+     * @throws \InvalidArgumentException when the first param of the relation is not a relation type constant,
+     * or there's one or more arguments missing
+     * @see Ardent::relationsData
+     */
+    protected function handleRelationalArray($relationName) {
+        $relation     = static::$relationsData[$relationName];
+        $relationType = $relation[0];
+        $errorHeader = "Relation '$relationName' on model '".get_called_class();
+
+        if (!in_array($relationType, static::$relationTypes)) {
+            throw new \InvalidArgumentException($errorHeader.' should have as first param one of the relation constants of the Ardent class.');
+        }
+        if (!isset($relation[1]) && $relationType != self::MORPH_TO) {
+            throw new \InvalidArgumentException($errorHeader.' should have at least two params: relation type and classname.');
+        }
+        if (isset($relation[1]) && $relationType == self::MORPH_TO) {
+            throw new \InvalidArgumentException($errorHeader.' is a morphTo relation and should not contain additional arguments.');
+        }
+
+        $verifyArgs = function (array $optional, array $required = array()) use ($relationName, &$relation, $errorHeader) {
+            $missing = array('required' => array(), 'optional' => array());
+
+            foreach(array('required', 'optional') as $keyType) {
+                foreach ($$keyType as $key) {
+                    if (!array_key_exists($key, $relation)) {
+                        $missing[$keyType][] = $key;
+                    }
+                }
+            }
+
+            if ($missing['required']) {
+                throw new \InvalidArgumentException($errorHeader.'
+                    should contain the following key(s): '.join(', ', $missing['required']));
+            }
+            if ($missing['optional']) {
+                foreach ($missing['optional'] as $include) {
+                    $relation[$include] = null;
+                }
+            }
+        };
+
+        switch ($relationType) {
+            case self::HAS_ONE:
+            case self::HAS_MANY:
+            case self::BELONGS_TO:
+                $verifyArgs(array('foreignKey'));
+                return $this->$relationType($relation[1], $relation['foreignKey']);
+
+            case self::BELONGS_TO_MANY:
+                $verifyArgs(array('table', 'foreignKey', 'otherKey'));
+                return $this->$relationType($relation[1], $relation['table'], $relation['foreignKey'],
+                    $relation['otherKey']);
+
+            case self::MORPH_TO:
+                $verifyArgs(array('name', 'type', 'id'));
+                return $this->$relationType($relation['name'], $relation['type'], $relation['id']);
+
+            case self::MORPH_ONE:
+            case self::MORPH_MANY:
+                $verifyArgs(array('type', 'id'), array('name'));
+                return $this->$relationType($relation[1], $relation['name'], $relation['type'], $relation['id']);
+        }
+    }
+
+
+    /**
+     * Handle dynamic method calls into the method.
+     * Overrided from {@link Eloquent} to implement recognition of the {@link $relationsData} array.
+     *
+     * @param  string  $method
+     * @param  array   $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters) {
+        if (array_key_exists($method, static::$relationsData)) {
+            return $this->handleRelationalArray($method);
+        }
+
+        return parent::__call($method, $parameters);
+    }
+
+
+    /**
+     * Get an attribute from the model.
+     * Overrided from {@link Eloquent} to implement recognition of the {@link $relationsData} array.
+     *
+     * @param  string  $key
+     * @return mixed
+     */
+    public function getAttribute($key) {
+        $attr = parent::getAttribute($key);
+
+        if ($attr === null) {
+            $camelKey = camel_case($key);
+            if (array_key_exists($camelKey, static::$relationsData)) {
+                $this->relations[$key] = $this->$camelKey()->getResults();
+                return $this->relations[$key];
+            }
+        }
+
+        return $attr;
     }
 
     /**
@@ -251,6 +421,7 @@ abstract class Ardent extends Model {
      *
      * @param bool $success Indicates whether the database save operation succeeded
      * @param bool $forced  Indicates whether the model is being saved forcefully
+     *
      * @return void
      */
     protected function afterSave($success, $forced = false) {
