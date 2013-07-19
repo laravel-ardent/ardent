dcbf1bcfda4e7238f4688056ad2de5d8ce192045
Igor Santos <igorsantos07@gmail.com>
2013-07-19 14:34:28 -0300

    Allowing for the query builder to inherit the model's throwOnFind property, thus enabling multi-level queries to throw exceptions as well (such as `User::with('relation')->first()`).

--- src/LaravelBook/Ardent/Ardent.php
+++ src/LaravelBook/Ardent/Ardent.php
@@ -796,4 +796,28 @@ abstract class Ardent extends Model {
             return parent::find($id, $columns);
         }
     }
+
+	/**
+	 * Get a new query builder for the model's table.
+	 * Overriden from {@link \Model\Eloquent} to allow for usage of {@link throwOnFind}.
+	 *
+	 * @param  bool  $excludeDeleted
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
+	public function newQuery($excludeDeleted = true) {
+		$builder = new Builder($this->newBaseQueryBuilder());
+		$builder->throwOnFind = static::$throwOnFind;
+
+		// Once we have the query builders, we will set the model instances so the
+		// builder can easily access any information it may need from the model
+		// while it is constructing and executing various queries against it.
+		$builder->setModel($this)->with($this->with);
+
+		if ($excludeDeleted and $this->softDelete)
+		{
+			$builder->whereNull($this->getQualifiedDeletedAtColumn());
+		}
+
+		return $builder;
+	}
 }
