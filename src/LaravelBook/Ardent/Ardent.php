29addaf853b3a5b3f91cec5e689fc3117d96e750
Igor Santos <igorsantos07@gmail.com>
2013-07-20 00:41:28 -0300

    General code cleanup
    - removal of blank before/afterSave methods, as having blank methods for all hooks would clutter the class
    * forceSave() now calls save(), that have an additional  argument [DRY]
    + minor improvements on methods documentation

--- src/LaravelBook/Ardent/Ardent.php
+++ src/LaravelBook/Ardent/Ardent.php
@@ -182,7 +182,7 @@ abstract class Ardent extends Model {
 
     /**
      * Array of relations used to verify arguments used in the {@link $relationsData}
-     * 
+     *
      * @var array
      */
     protected static $relationTypes = array(
@@ -210,28 +210,31 @@ abstract class Ardent extends Model {
      * @param $relationName the relation key, camel-case version
      * @return \Illuminate\Database\Eloquent\Relations\Relation
      * @throws \InvalidArgumentException when the first param of the relation is not a relation type constant,
-     * or there's one or more arguments missing
+     *      or there's one or more arguments missing
      * @see Ardent::relationsData
      */
     protected function handleRelationalArray($relationName) {
         $relation     = static::$relationsData[$relationName];
         $relationType = $relation[0];
-        $errorHeader = "Relation '$relationName' on model '".get_called_class();
+        $errorHeader  = "Relation '$relationName' on model '".get_called_class();
 
         if (!in_array($relationType, static::$relationTypes)) {
-            throw new \InvalidArgumentException($errorHeader.' should have as first param one of the relation constants of the Ardent class.');
+            throw new \InvalidArgumentException($errorHeader.
+            ' should have as first param one of the relation constants of the Ardent class.');
         }
         if (!isset($relation[1]) && $relationType != self::MORPH_TO) {
-            throw new \InvalidArgumentException($errorHeader.' should have at least two params: relation type and classname.');
+            throw new \InvalidArgumentException($errorHeader.
+            ' should have at least two params: relation type and classname.');
         }
         if (isset($relation[1]) && $relationType == self::MORPH_TO) {
-            throw new \InvalidArgumentException($errorHeader.' is a morphTo relation and should not contain additional arguments.');
+            throw new \InvalidArgumentException($errorHeader.
+            ' is a morphTo relation and should not contain additional arguments.');
         }
 
-        $verifyArgs = function (array $optional, array $required = array()) use ($relationName, &$relation, $errorHeader) {
+        $verifyArgs = function (array $opt, array $req = array()) use ($relationName, &$relation, $errorHeader) {
             $missing = array('required' => array(), 'optional' => array());
 
-            foreach(array('required', 'optional') as $keyType) {
+            foreach (array('req', 'opt') as $keyType) {
                 foreach ($$keyType as $key) {
                     if (!array_key_exists($key, $relation)) {
                         $missing[$keyType][] = $key;
@@ -273,13 +276,12 @@ abstract class Ardent extends Model {
         }
     }
 
-
     /**
      * Handle dynamic method calls into the method.
      * Overrided from {@link Eloquent} to implement recognition of the {@link $relationsData} array.
      *
-     * @param  string  $method
-     * @param  array   $parameters
+     * @param  string $method
+     * @param  array  $parameters
      * @return mixed
      */
     public function __call($method, $parameters) {
@@ -290,12 +292,11 @@ abstract class Ardent extends Model {
         return parent::__call($method, $parameters);
     }
 
-
     /**
      * Get an attribute from the model.
      * Overrided from {@link Eloquent} to implement recognition of the {@link $relationsData} array.
      *
-     * @param  string  $key
+     * @param  string $key
      * @return mixed
      */
     public function getAttribute($key) {
@@ -336,6 +337,16 @@ abstract class Ardent extends Model {
         self::$validationFactory = new ValidationFactory($translator);
     }
 
+    /**
+     * Instatiates the validator used by the validation process, depending if the class is being used inside or
+     * outside of Laravel.
+     *
+     * @param $data
+     * @param $rules
+     * @param $customMessages
+     * @return \Illuminate\Validation\Validator
+     * @see Ardent::$externalValidator
+     */
     protected static function makeValidator($data, $rules, $customMessages) {
         if (self::$externalValidator) {
             return self::$validationFactory->make($data, $rules, $customMessages);
@@ -446,56 +457,52 @@ abstract class Ardent extends Model {
      * @param array   $options
      * @param Closure $beforeSave
      * @param Closure $afterSave
+     * @param bool    $force Forces saving invalid data. Defaults to false; when true has the same effect as calling
+     *                       {@link Ardent::forceSave()}.
      * @return bool
+     * @see Ardent::forceSave()
      */
     public function save(array $rules = array(),
         array $customMessages = array(),
         array $options = array(),
         Closure $beforeSave = null,
-        Closure $afterSave = null) {
+        Closure $afterSave = null,
+        $force = false
+    ) {
+        if ($beforeSave) {
+            self::saving($beforeSave);
+        }
+        if ($afterSave) {
+            self::saved($afterSave);
+        }
 
-        // validate
-        $validated = $this->validate($rules, $customMessages);
+        $valid = $this->validate($rules, $customMessages);
 
-        // execute beforeSave callback
-        $proceed = is_null($beforeSave)? $this->beforeSave(false) : $beforeSave($this);
-
-        // attempt to save if all conditions are satisfied
-        $success = ($proceed && $validated)? $this->performSave($options) : false;
-
-        is_null($afterSave)? $this->afterSave($success, false) : $afterSave($this);
-
-        return $success;
+        if ($force || $valid) {
+            return $this->performSave($options);
+        } else {
+            return false;
+        }
     }
 
     /**
      * Force save the model even if validation fails.
      *
-     * @param array   $rules         :array
-     * @param array   $customMessages:array
+     * @param array   $rules
+     * @param array   $customMessages
      * @param array   $options
      * @param Closure $beforeSave
      * @param Closure $afterSave
      * @return bool
+     * @see Ardent::save()
      */
     public function forceSave(array $rules = array(),
         array $customMessages = array(),
         array $options = array(),
         Closure $beforeSave = null,
-        Closure $afterSave = null) {
-
-        // validate the model
-        $this->validate($rules, $customMessages);
-
-        // execute beforeForceSave callback
-        $proceed = is_null($beforeSave)? $this->beforeSave(true) : $beforeSave($this);
-
-        // attempt to save regardless of the outcome of validation
-        $success = $proceed? $this->performSave($options) : false;
-
-        is_null($afterSave)? $this->afterSave($success, true) : $afterSave($this);
-
-        return $success;
+        Closure $afterSave = null
+    ) {
+        return $this->save($rules, $customMessages, $options, $beforeSave, $afterSave, true);
     }
 
     /**
@@ -672,8 +679,8 @@ abstract class Ardent extends Model {
         array $customMessages = array(),
         array $options = array(),
         Closure $beforeSave = null,
-        Closure $afterSave = null) {
-
+        Closure $afterSave = null
+    ) {
         // Only automatically modify rules if there are none coming in
         if (count($rules == 0)) {
             $rules = $this->buildUniqueExclusionRules();
