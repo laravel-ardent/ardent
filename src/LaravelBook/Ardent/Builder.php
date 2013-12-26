1103d02dc9e32902b68429db89ad136a6fbf2942
Igor Santos <igorsantos07@gmail.com>
2013-12-26 00:21:44 -0200

    Code style and doc

--- src/LaravelBook/Ardent/Builder.php
+++ src/LaravelBook/Ardent/Builder.php
@@ -1,7 +1,6 @@
 <?php
 namespace LaravelBook\Ardent;
 
-
 class Builder extends \Illuminate\Database\Eloquent\Builder {
 
 	/**
@@ -20,14 +19,16 @@ class Builder extends \Illuminate\Database\Eloquent\Builder {
 		return $this->maybeFail('first', func_get_args());
 	}
 
+	/**
+	 * Will test if it should run a normal method or its "orFail" version, and behave accordingly.
+	 * @param string $method called method
+	 * @param array  $args   given arguments
+	 * @return mixed
+	 */
 	protected function maybeFail($method, $args) {
-		$debug = debug_backtrace(false);
-
-		if ($this->throwOnFind && $debug[2]['function'] != "{$method}OrFail") {
-			return call_user_func_array(array($this, $method.'OrFail'), $args);
-		} else {
-			return call_user_func_array("parent::$method", $args);
-		}
+		$debug  = debug_backtrace(false);
+		$orFail = $method.'OrFail';
+		$func   = ($this->throwOnFind && $debug[2]['function'] != $orFail)? array($this, $orFail) : "parent::$method";
+		return call_user_func_array($func, $args);
 	}
-
 }
--- src/LaravelBook/Ardent/InvalidModelException.php
+++ src/LaravelBook/Ardent/InvalidModelException.php
@@ -9,31 +9,27 @@ class InvalidModelException extends \RuntimeException {
 
 	/**
 	 * The invalid model.
-	 *
 	 * @var \LaravelBook\Ardent\Ardent
 	 */
 	protected $model;
 
 	/**
 	 * The message bag instance containing validation error messages
-	 *
 	 * @var \Illuminate\Support\MessageBag
 	 */
 	protected $errors;
 
 	/**
 	 * Receives the invalid model and sets the {@link model} and {@link errors} properties.
-	 *
 	 * @param Ardent $model The troublesome model.
 	 */
-	public function __construct( Ardent $model ) {
-		$this->model = $model;
+	public function __construct(Ardent $model) {
+		$this->model  = $model;
 		$this->errors = $model->errors();
 	}
 
 	/**
 	 * Returns the model with invalid attributes.
-	 *
 	 * @return Ardent
 	 */
 	public function getModel() {
@@ -42,11 +38,9 @@ class InvalidModelException extends \RuntimeException {
 
 	/**
 	 * Returns directly the message bag instance with the model's errors.
-	 *
 	 * @return \Illuminate\Support\MessageBag
 	 */
 	public function getErrors() {
 		return $this->errors;
 	}
-
 }
