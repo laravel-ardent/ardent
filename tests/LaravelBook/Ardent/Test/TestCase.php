850bda92e565d1d94eacca9ddbb31780e9840550
Clark Fischer <clark.fischer@gmail.com>
2014-01-11 12:00:17 -0800

    Added Test Suite Boilerplate
    Added a 'bootstrap' file, which loads the autoloader, and adds the
    `LaravelBook\Ardent\Test` namespace to the environment.  Added a base
    `TestCase` class to be extended project-wide.

---
+++ tests/LaravelBook/Ardent/Test/TestCase.php
@@ -0,0 +1,7 @@
+<?php namespace LaravelBook\Ardent\Test;
+
+use PHPUnit_Framework_TestCase;
+
+class TestCase extends PHPUnit_Framework_TestCase
+{
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,6 @@
+<?php
+
+error_reporting(E_ALL);
+
+$loader = require __DIR__.'/../vendor/autoload.php';
+$loader->add('LaravelBook\Ardent\Test', __DIR__);
