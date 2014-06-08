d3279d2d8be24664b0d5b5ffca47317890c704f8
Casper Lai <casper.lai@sleepingdesign.com>
2014-06-09 01:46:02 +0800

    Fix soft deleted doesn't work when upgraded Laravel 4.2

--- src/LaravelBook/Ardent/Ardent.php
+++ src/LaravelBook/Ardent/Ardent.php
@@ -882,11 +882,6 @@ abstract class Ardent extends Model {
 		// while it is constructing and executing various queries against it.
 		$builder->setModel($this)->with($this->with);
 
-		if ($excludeDeleted and $this->softDelete)
-		{
-			$builder->whereNull($this->getQualifiedDeletedAtColumn());
-		}
-
-		return $builder;
+		return $this->applyGlobalScopes($builder);
 	}
 }
