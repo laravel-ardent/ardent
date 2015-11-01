ca461981af0fc52f271484d2e85fa9b9f7fe89d2
Igor Santos <igorsantos07@gmail.com>
2015-11-01 19:29:49 -0200

    Removing file docblocks
    They had information that's outdated, or available elsewhere

--- src/Ardent/Ardent.php
+++ src/Ardent/Ardent.php
@@ -1,14 +1,5 @@
 <?php namespace LaravelArdent\Ardent;
 
-/*
- * This file is part of the Ardent package.
- *
- * (c) Max Ehsan <contact@LaravelArdent.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use Closure;
 use Illuminate\Container\Container;
 use Illuminate\Database\Capsule\Manager as DatabaseCapsule;
--- src/Ardent/Facades/Ardent.php
+++ src/Ardent/Facades/Ardent.php
@@ -1,14 +1,5 @@
 <?php namespace LaravelArdent\Ardent\Facades;
 
-/*
- * This file is part of the Ardent package.
- *
- * (c) Max Ehsan <contact@LaravelArdent.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
-
 use Illuminate\Support\Facades\Facade;
 
 class Ardent extends Facade {
